a04c2828bea8eae9c19f5472913e5255714cd099
serbanoprea <serban.oprea@yahoo.com>
2019-10-26 13:51:14 +0100

    Try

---
+++ build.sh
@@ -0,0 +1,6 @@
+pip install awscli
+pip freeze
+awscli configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+awscli configure set secret_access_key $AWS_SECRET_ACCESS_KEY
+awscli configure set default.region eu-west-1
+aws s3 cp --recursive ./ s3://crypto-deployment-code/$APPVEYOR_REPO_BRANCH
