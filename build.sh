f9733f373a76c800043a2cc88f492da9e3ca7686
Serban Oprea <serban.oprea1@gmail.com>
2019-10-26 13:26:01 +0100

    Adds build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+pip install awscli
+pip freeze
+
+# Configure AWS
+awscli configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+awscli configure set secret_access_key $AWS_SECRET_ACCESS_KEY
+awscli configure set default.region eu-west-1
+
+aws s3 cp --recursive ./ s3://crypto-deployment-code/$APPVEYOR_REPO_BRANCH
