29ffaeb22a0cce8f82cd9a09636c7f492a9df83d
serbanoprea <serban.oprea@yahoo.com>
2019-10-26 14:15:26 +0100

    Don't copy .git

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ pip freeze
 awscli configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 awscli configure set secret_access_key $AWS_SECRET_ACCESS_KEY
 awscli configure set default.region eu-west-1
-aws s3 cp --recursive ./ s3://s3-crypto-deployment-code/$APPVEYOR_REPO_BRANCH
+aws s3 cp --recursive ./ s3://s3-crypto-deployment-code/$APPVEYOR_REPO_BRANCH --exclude .git
