2e4c9204292537b20f703d3f24f09eff61b945d3
serbanoprea <serban.oprea@yahoo.com>
2020-07-03 19:16:13 +0100

    Quick master fix

--- pipeline/data_aggregation/output_db_aggregates.py
+++ pipeline/data_aggregation/output_db_aggregates.py
@@ -1,11 +1,8 @@
-from datetime import datetime, time, timedelta
-
 import luigi
-from luigi.tools.range import RangeHourly
 
 from pipeline.common.read import get_table_columns, generate_analytics_path
 from pipeline.common.tasks import OutputDatabaseTask
-from pipeline.database.aggregates_insert import InsertPopulationAggregates, InsertCoinAggregates
+from pipeline.database.aggregates_insert import InsertCoinAggregates
 from pipeline.database.trends_insert import InsertDailyTrends
 
 _config = luigi.configuration.get_config()
@@ -14,28 +11,6 @@ _hourly_trends_table = _config.get('database', 'hourly-trends-table')
 _daily_trends_table = _config.get('database', 'daily-trends-table')
 _coin_aggregates_table = _config.get('database', 'coin-aggregates-table')
 _group = 'db-aggregations'
-
-#
-# class OutputHourlyTrends(OutputDatabaseTask):
-#     date = luigi.DateParameter()
-#
-#     def requires(self):
-#         return RangeHourly(
-#             of=InsertHourlyTrends,
-#             start=datetime.combine(self.date, time.min),
-#             stop=datetime.combine(self.date + timedelta(days=1), time.min)
-#         )
-#
-#     @property
-#     def output_path(self):
-#         return generate_analytics_path(
-#             group=_group,
-#             name='hourly-trends',
-#             date=self.date
-#         )
-#
-#     table = _hourly_trends_table
-#     columns = get_table_columns(_hourly_trends_table)
 
 
 class OutputDailyTrends(OutputDatabaseTask):
@@ -75,31 +50,12 @@ class OutputCoinAggregations(OutputDatabaseTask):
     columns = get_table_columns(_coin_aggregates_table)
 
 
-class OutputPopulationAggregations(OutputDatabaseTask):
-    date = luigi.DateParameter()
-
-    def requires(self):
-        return InsertPopulationAggregates(**self.param_kwargs)
-
-    @property
-    def output_path(self):
-        return (
-            generate_analytics_path(group=_group,
-                                    name='population-aggregations',
-                                    date=self.date)
-        )
-
-    table = _population_aggregates_table
-    columns = get_table_columns(_population_aggregates_table)
-
-
 class DailyDbAggregatesOutput(luigi.WrapperTask):
     date = luigi.DateParameter()
 
     def requires(self):
         # yield OutputHourlyTrends(**self.param_kwargs)
         yield OutputDailyTrends(**self.param_kwargs)
-        yield OutputPopulationAggregations(**self.param_kwargs)
 
 
 class HourlyDbAggregatesOutput(luigi.WrapperTask):
